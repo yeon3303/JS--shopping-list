ebcb0e073bb6fb98777716963d84c7d08793f2a7
yeon3303 <yeon3303@gmail.com>
2021-04-23 15:42:17 +0900

    modify codes

--- main.js
+++ main.js
@@ -1,31 +1,72 @@
 'use strict';
 
-const input = document.querySelector('input');
-const btnClear = document.querySelector('.btnClear');
-const main = document.querySelector('main');
-const form = document.querySelector('form');
+const input__text = document.querySelector('.input__text');
+const addBtn = document.querySelector('.addBtn');
+const lists = document.querySelector('.lists');
+const clrBtn = document.querySelector('.clrBtn');
 
-function addList(e) {
-    const inputValue = input.value;
-    const list = document.createElement('li');
-    const del = document.createElement('span');
-    list.setAttribute('class', 'list');
-    list.textContent = inputValue;
-    del.textContent = '🗑';
-    main.appendChild(list);
-    list.appendChild(del);
-    e.preventDefault();
+function onAdd() {
+    const text = input__text.value;
 
-    del.addEventListener('click', function delList(e) {
-        main.removeChild(list);
-        e.preventDefault();
-    });
+    if (text !== '') {
+        createList(text);
+    }
+
+    input__text.value = '';
+    input__text.focus();
 };
 
-form.addEventListener('submit', addList);
+function createList(text) {
+    const list = document.createElement('li');
+    list.setAttribute('class', 'list');
 
-btnClear.addEventListener('click', init);
+    const listCheckItem = document.createElement('div');
+    listCheckItem.setAttribute('class', 'list__check__item');
 
-function init() {
+    const listCheck = document.createElement('span');
+    listCheck.setAttribute('class', 'list__check');
+    listCheck.innerHTML = '<i class="far fa-check-circle"></i>';
+    
+    const listItem = document.createElement('span');
+    listItem.setAttribute('class', 'list__item');
+    listItem.textContent = text;
+
+    const listDelBtn = document.createElement('div');
+    listDelBtn.setAttribute('class', 'list__delBtn');
+    listDelBtn.innerHTML = '<i class="far fa-trash-alt"></i>';
+    
+    listCheckItem.appendChild(listCheck);
+    listCheckItem.appendChild(listItem);
+    list.appendChild(listCheckItem);
+    list.appendChild(listDelBtn);
+
+    lists.appendChild(list);
+    list.scrollIntoView({block: "center"});
+
+    listDelBtn.addEventListener('click', () => {
+        lists.removeChild(list);
+    });
+
+    list.addEventListener('click', () => {
+        list.style.textDecoration = "line-through";
+        listCheck.style.color = "green";
+    });
+    
+    return list;
+};
+
+
+addBtn.addEventListener('click', () => {
+    onAdd();
+});
+
+input__text.addEventListener('keyup', event => {
+    if (event.keyCode === 13) {
+        onAdd();
+    }
+})
+
+clrBtn.addEventListener('click', () => {
     window.location.reload();
-}
+})
+
